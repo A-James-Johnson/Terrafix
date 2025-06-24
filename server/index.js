06b8e7f9ccc190df14dede4e15c5a39e4ff71bd4
A-James-Johnson <jamesjohnson.a2022it@sece.ac.in>
2025-06-24 11:30:52 +0530

    final code

--- server/index.js
+++ server/index.js
@@ -44,12 +44,12 @@ app.use("/", terraformRoutes);
 
 
 
-// Serve static files from Vite build
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+// // Serve static files from Vite build
+// app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+// });
 
 
 
